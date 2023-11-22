c4c01faf2841bb58ae62f27c5d6f0335074de3f6
BALARAM BADAVATH <badavathbalaram17@gmail.com>
2023-11-22 15:54:24 +0530

    Initial commit

---
+++ JAVA SCRIPT/ways.js
@@ -0,0 +1,12 @@
+a=10;
+b=20.5;
+c='hello';
+d=true
+console.log(a)
+console.log(typeof a)
+console.log(b)
+console.log(typeof b)
+console.log(c)
+console.log(typeof c)
+console.log(d)
+console.log(typeof d)
